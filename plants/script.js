bf373d603dc89f5d0b34c9284c97a1eefb18e7a9
Sweta <Stellla@tut.by>
2023-01-18 14:47:36 +0100

    feat: add menu burger

--- plants/script.js
+++ plants/script.js
@@ -3,3 +3,25 @@ console.log(`1. Вёрстка соответствует макету при ш
 3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. 
 Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15
 4. На ширине экрана 430рх и меньше реализовано адаптивное меню +22 `);
+
+const body = document.body;
+const burgerButton = document.getElementById("burger");
+const menu = document.getElementById("nav-list");
+const overlay = document.getElementById("overlay");
+const links = menu.querySelectorAll("li");
+
+const toggleMenu = () => {
+  body.classList.toggle("open-menu");
+  burgerButton.classList.toggle("open");
+  menu.classList.toggle("open");
+  overlay.classList.toggle("open");
+};
+burgerButton.onclick = toggleMenu;
+overlay.onclick = toggleMenu;
+links.forEach((link) => {
+  link.onclick = () => {
+    if (menu.classList.contains("open")) {
+      toggleMenu();
+    }
+  };
+});
