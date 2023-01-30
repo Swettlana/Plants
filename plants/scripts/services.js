df244bd7c071caab6773a6a325692078e06e207b
Sweta <Stellla@tut.by>
2023-01-30 22:38:00 +0100

    fix:fixed bugs with hover

---
+++ plants/scripts/services.js
@@ -0,0 +1,8 @@
+const NAME_CATEGORY = {
+  "Garden care": "Gardens",
+  Planting: "Planting",
+  "Lawn care": "Lawn",
+};
+
+const servicesButtons = document.getElementById("service-buttons");
+const serviceItems = document.querySelectorAll(".service-item");
