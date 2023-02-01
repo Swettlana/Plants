fef476bc384720bdf33ebe29fa2951c974196ade
Sweta <Stellla@tut.by>
2023-02-01 21:27:55 +0100

    fix: fixed styles and script for servises

--- plants/scripts/services.js
+++ plants/scripts/services.js
@@ -1,14 +1,10 @@
-const NAME_CATEGORY = {
-  "Garden care": "Gardens",
-  Planting: "Planting",
-  "Lawn care": "Lawn",
-};
-
-const servicesButtons = document.querySelectorAll(".service-button");
-const serviceItems = document.querySelectorAll(".service-item");
+const servicesButtons = Array.from(
+  document.querySelectorAll(".service-button")
+);
+const serviceItems = Array.from(document.querySelectorAll(".service-item"));
 
 const countPress = () => {
-  return Array.from(servicesButtons).reduce(
+  return servicesButtons.reduce(
     (count, button) =>
       button.classList.contains("active") ? count + 1 : count,
     0
@@ -17,11 +13,9 @@ const countPress = () => {
 
 const changeClassActive = (button) => {
   button.classList.toggle("active");
-
-  const activeButtons = Array.from(servicesButtons)
-    .filter((item) => item.classList.contains("active"))
+  const activeButtons = servicesButtons
+    .filter((button) => button.classList.contains("active"))
     .map((button) => (button = button.dataset.category));
-  console.log(activeButtons);
   serviceItems.forEach((card) => card.classList.remove("blur"));
   if (activeButtons.length !== 0) {
     serviceItems.forEach((card) => {
@@ -35,10 +29,7 @@ const changeClassActive = (button) => {
 servicesButtons.forEach(
   (button) =>
     (button.onclick = () => {
-      if (button.classList.contains("active")) {
+      if (countPress() !== 2 || button.classList.contains("active"))
         changeClassActive(button);
-      } else if (countPress(servicesButtons) !== 2) {
-        changeClassActive(button);
-      }
     })
 );
