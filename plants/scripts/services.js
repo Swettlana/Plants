c315070dd0cdc96623c38afed29ceb6fecad58e7
Sweta <Stellla@tut.by>
2023-02-01 20:44:18 +0100

    feat:add functional in block servises

--- plants/scripts/services.js
+++ plants/scripts/services.js
@@ -4,5 +4,41 @@ const NAME_CATEGORY = {
   "Lawn care": "Lawn",
 };
 
-const servicesButtons = document.getElementById("service-buttons");
+const servicesButtons = document.querySelectorAll(".service-button");
 const serviceItems = document.querySelectorAll(".service-item");
+
+const countPress = () => {
+  return Array.from(servicesButtons).reduce(
+    (count, button) =>
+      button.classList.contains("active") ? count + 1 : count,
+    0
+  );
+};
+
+const changeClassActive = (button) => {
+  button.classList.toggle("active");
+
+  const activeButtons = Array.from(servicesButtons)
+    .filter((item) => item.classList.contains("active"))
+    .map((button) => (button = button.dataset.category));
+  console.log(activeButtons);
+  serviceItems.forEach((card) => card.classList.remove("blur"));
+  if (activeButtons.length !== 0) {
+    serviceItems.forEach((card) => {
+      if (!activeButtons.includes(card.dataset.category)) {
+        card.classList.add("blur");
+      }
+    });
+  }
+};
+
+servicesButtons.forEach(
+  (button) =>
+    (button.onclick = () => {
+      if (button.classList.contains("active")) {
+        changeClassActive(button);
+      } else if (countPress(servicesButtons) !== 2) {
+        changeClassActive(button);
+      }
+    })
+);
