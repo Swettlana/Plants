107af63e93dd814d6142f6770107fbac01e957e6
Sweta <Stellla@tut.by>
2023-02-03 00:02:06 +0100

    fix: fixed accordion

--- plants/scripts/accordion.js
+++ plants/scripts/accordion.js
@@ -1,7 +1,34 @@
 const buttons = document.querySelectorAll(".accordion-switch");
+let countActive = 0;
+
+const switchAccordion = (el) => {
+  el.parentNode.classList.toggle("active");
+  el.parentNode.nextElementSibling.classList.toggle("active");
+};
+
+const closeAccordion = (el) => {
+  el.parentNode.classList.remove("active");
+  el.parentNode.nextElementSibling.classList.remove("active");
+  el.dataset.open = "false";
+};
+
 buttons.forEach((button) => {
   button.onclick = () => {
-    button.parentNode.classList.toggle("active");
-    button.parentNode.nextElementSibling.classList.toggle("active");
+    if (button.dataset.open == "true") {
+      switchAccordion(button);
+      button.dataset.open = "false";
+      countActive = 0;
+    } else {
+      if (countActive) {
+        buttons.forEach((item) => {
+          if (item.dataset.open == "true") {
+            closeAccordion(item);
+          }
+        });
+      }
+      switchAccordion(button);
+      button.dataset.open = "true";
+      countActive = 1;
+    }
   };
 });
